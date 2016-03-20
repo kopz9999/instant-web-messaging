b229776bb71aac240fa17a70b5f327a596f288e3
Kyoto Kopz <kopz9999@gmail.com>
2016-03-20 02:48:08 -0700

    configure server

--- server.js
+++ server.js
@@ -1,27 +1,42 @@
 var path = require('path');
 var webpack = require('webpack');
 var express = require('express');
-var config = require('./webpack.config');
 
 var app = express();
-var compiler = webpack(config);
+var isDevelopment = (process.env.NODE_ENV !== 'production');
+var static_path = path.join(__dirname, 'static');
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath,
-  historyApiFallback: true
-}));
+if (isDevelopment) {
+  var config = require('./webpack.config');
+  var compiler = webpack(config);
 
-app.use(require('webpack-hot-middleware')(compiler));
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath,
+    historyApiFallback: true
+  }));
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-  
-app.listen(3000, 'localhost', function (err, result) {
-  if (err) {
-    console.log(err);
-  }
+  app.use(require('webpack-hot-middleware')(compiler));
 
-  console.log('Listening at localhost:3000');
-});
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+  });
+
+  app.listen(3000, 'localhost', function (err, result) {
+    if (err) {
+      console.log(err);
+    }
+
+    console.log('Listening at localhost:3000');
+  });
+} else {
+  app.use(express.static(static_path))
+    .get('/', function (req, res) {
+      res.sendFile('index.html', {
+        root: static_path
+      });
+    }).listen(process.env.PORT || 8080, function (err) {
+    if (err) { console.log(err) };
+    console.log('Listening at localhost:8080');
+  });
+}
