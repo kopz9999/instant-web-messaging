d0dc20ff359d61983a86d99314123ef3cd020f28
Kyoto Kopz <Kyoto Kopz>
2016-04-13 01:17:59 -0700

    Vagrant updated

--- install-nvm.sh
+++ install-nvm.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get install build-essential libssl-dev
 sudo apt-get install -y git-core curl
-curl https://raw.githubusercontent.com/creationix/nvm/v0.23.3/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh | bash
