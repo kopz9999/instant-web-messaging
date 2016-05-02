e4ca4c442e161c3a853739ce7c0899b3e12068fb
Kyoto Kopz <Kyoto Kopz>
2016-05-02 03:55:10 -0700

    fixed devTools bug

--- js/utils/DevTools.js
+++ js/utils/DevTools.js
@@ -1,24 +1,29 @@
 import React from 'react';
 
-// Exported from redux-devtools
-import { createDevTools } from 'redux-devtools';
+let DevTools;
 
-// Monitors are separate packages, and you can make a custom one
-import LogMonitor from 'redux-devtools-log-monitor';
-import DockMonitor from 'redux-devtools-dock-monitor';
+if (__DEV__) {
+  // Exported from redux-devtools
+  let { createDevTools } = require('redux-devtools');
 
-// createDevTools takes a monitor and produces a DevTools component
-const DevTools = createDevTools(
-  // Monitors are individually adjustable with props.
-  // Consult their repositories to learn about those props.
-  // Here, we put LogMonitor inside a DockMonitor.
-  // Note: DockMonitor is visible by default.
-  <DockMonitor toggleVisibilityKey='ctrl-h'
-               changePositionKey='ctrl-q'
-               defaultIsVisible={true}
-               defaultPosition='left'>
-    <LogMonitor theme='tomorrow' />
-  </DockMonitor>
-);
+  // Monitors are separate packages, and you can make a custom one
+  let LogMonitor = require('redux-devtools-log-monitor').default;
+  let DockMonitor = require('redux-devtools-dock-monitor').default;
+
+  DevTools = createDevTools(
+    // Monitors are individually adjustable with props.
+    // Consult their repositories to learn about those props.
+    // Here, we put LogMonitor inside a DockMonitor.
+    // Note: DockMonitor is visible by default.
+    <DockMonitor toggleVisibilityKey='ctrl-h'
+                 changePositionKey='ctrl-q'
+                 defaultIsVisible={true}
+                 defaultPosition='left'>
+      <LogMonitor theme='tomorrow' />
+    </DockMonitor>
+  );
+} else {
+  DevTools = () => (<div style={{display: 'none'}}></div>);
+}
 
 export default DevTools;
