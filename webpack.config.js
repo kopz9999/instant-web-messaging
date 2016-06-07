c39bd3689504eafb29c874b091eabe60b344c735
Kyoto Kopz <kopz9999@gmail.com>
2016-06-07 14:43:17 -0700

    0.2.1 # increase image size for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
     loaders: [
       { test: /\.js$/, loaders: ['babel'], exclude: /node_modules/ },
       { test: /\.css$/, loader: 'style!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]' },
-      { test: /\.png$/, loader: 'url-loader?limit=10000&mimetype=image/png' }
+      { test: /\.png$/, loader: 'url-loader?limit=20000&mimetype=image/png' }
     ]
   },
   resolve: {
