0df725062685fa9551aa48a3f1b3f62ee6adf265
Kyoto Kopz <kopz9999@gmail.com>
2016-06-24 14:50:17 -0700

    hotfix # update notifications example

--- examples/notifications.js
+++ examples/notifications.js
@@ -1,25 +1,6 @@
-document.addEventListener("DOMContentLoaded", function() {
+function initializeApp() {
   // Layer custom variables
   var launcherNode = jQuery('#chat-app-trigger');
-
-  clientUser = new webMessenger.User({
-    displayName: 'Margaret Bell',
-    roleName: 'Product support',
-    layerId: 'Customer Support',
-    avatarURL: 'https://s3-us-west-2.amazonaws.com/kopz-projects/Curaytor/Messenger/admin-avatar.png'
-  });
-
-  // Showing example with custom class and id
-  consumerUser = {
-    id: '502c0d48-8ae8-492e-8ec3-4d8b1d48d6f8',
-    displayName: 'Visitor',
-    layerId: 'Visitor',
-  };
-
-  if (QueryString.username) {
-    consumerUser.displayName = consumerUser.layerId =
-      decodeURIComponent(QueryString.username.replace(/\+/g, '%20'));
-  }
 
   messengerApp = webMessenger.createApp({
     launcherElement: launcherNode.get(0),
@@ -38,4 +19,39 @@ document.addEventListener("DOMContentLoaded", function() {
     window.open('./fullscreen.html', 'messenger');
     return true;
   });
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  var http = new XMLHttpRequest();
+
+  // Showing example with custom class and id
+  var userData = {
+    id: '502c0d48-8ae8-492e-8ec3-4d8b1d48d6f8',
+    displayName: 'Visitor',
+    layerId: 'Visitor',
+  };
+
+  clientUser = new webMessenger.User({
+    displayName: 'Margaret Bell',
+    roleName: 'Product support',
+    layerId: 'Customer Support',
+    avatarURL: 'https://s3-us-west-2.amazonaws.com/kopz-projects/Curaytor/Messenger/admin-avatar.png'
+  });
+
+  if (QueryString.username) {
+    userData.displayName = userData.layerId = userData.id =
+      decodeURIComponent(QueryString.username.replace(/\+/g, '%20'));
+  }
+
+  http.open("POST", usersAPI, true);
+  http.setRequestHeader("Content-type", "application/json");
+
+  http.onreadystatechange = function() {//Call a function when the state changes.
+    if(http.readyState == 4 && http.status == 200) {
+      consumerUser = new webMessenger.User(JSON.parse(http.responseText));
+      initializeApp();
+    }
+  };
+
+  http.send(JSON.stringify(userData));
 });
